3f6595b08814b7a3d2269956005af668bce67557
samhhenderson <yourname@example.com>
2022-08-02 14:47:10 -0700

    Integrate SASS. Reorganize styles per SASS guidelines. Finalize wording. Include different layout for small screens. Finalized except for form validation.

---
+++ script.js
@@ -0,0 +1,16 @@
+//Set variables and constants
+
+
+//Create some links to elements
+const firstNameWarning = document.querySelector(`#first-name-warning`);
+const lastNameWarning = document.querySelector(`#last-name-warning`);
+const emailWarning = document.querySelector(`#emaily-warning`);
+const phoneWarning = document.querySelector(`#phone-warning`);
+const passwordWarning = document.querySelector(`#password-warning`);
+const passwordCheckWarning = document.querySelector(`#password-check-warning`);
+const submit = document.querySelector(`button`);
+
+//Disable submit button after pressing;
+/* submit.addEventListener(`click`, () => submit.disabled = true); */
+
+//Ready to add complex validation
